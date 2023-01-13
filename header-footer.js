6bb62d155205d0081ffaa8d7d422a48ce53893a0
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-01-12 23:22:14 -0700

    Added header stuff

--- header-footer.js
+++ header-footer.js
@@ -1,3 +1,15 @@
 function openDropdown() {
     document.getElementById("dropdownID").classList.add('dropdownOpenAnimation')
 }
+
+function deleteTitle(windowWidth) {
+    if(windowWidth.matches){
+        document.getElementById("titleID").remove();
+    } else {
+        document.getElementById("titleID").innerHTML = "<h1 class='headerTitle'><span class='dragonSpanTitle'>Jī lóng</span> Grill</h1>";
+    }
+}
+
+var windowWidth = window.matchMedia("(max-width: 780px)")
+deleteTitle(windowWidth);
+windowWidth.addEventListener(deleteTitle)

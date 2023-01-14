f367dae67d7c9f16850d8fddf20767375e2a54a8
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2023-01-14 15:56:48 -0700

    Finished Header and Dropdown Menu

--- header-footer.js
+++ header-footer.js
@@ -3,15 +3,19 @@ let dropdownOpened = false;
 function openDropdown() {
     console.log('worked')
     if (dropdownOpened == false){
-        document.getElementById("dropdownID").classList.remove('dropdownCloseAnimation');
-        document.getElementById("dropdownID").classList.add('dropdownOpenAnimation');
+        document.getElementById("dropdownContentID").classList.remove('dropdownCloseAnimation');
+        document.getElementById("dropdownContentID").classList.add('dropdownOpenAnimation');
+        
+        document.getElementById("arrowID").classList.remove('arrowCloseAnimation');
+        document.getElementById("arrowID").classList.add('arrowOpenAnimation');
         dropdownOpened = true;
-        console.log(dropdownOpened)
     } else if(dropdownOpened == true){
-        document.getElementById("dropdownID").classList.remove('dropdownOpenAnimation');
-        document.getElementById("dropdownID").classList.add('dropdownCloseAnimation');
+        document.getElementById("dropdownContentID").classList.remove('dropdownOpenAnimation');
+        document.getElementById("dropdownContentID").classList.add('dropdownCloseAnimation');
+        
+        document.getElementById("arrowID").classList.remove('arrowOpenAnimation');
+        document.getElementById("arrowID").classList.add('arrowCloseAnimation');
         dropdownOpened = false;
-        console.log(dropdownOpened)
     }
 }
 
